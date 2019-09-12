415595eff10e293307a8ead0093447b4c71a22d2
ag <48479505+AGx10k@users.noreply.github.com>
2019-09-12 15:16:45 +0300

    Update random-giveaway.sh

--- AGx10k/random-giveaway.sh
+++ AGx10k/random-giveaway.sh
@@ -37,7 +37,7 @@ cd ${HARMONY_ROOT}
 wallet=$(cd ${HARMONY_ROOT}; LD_LIBRARY_PATH=. ./wallet -p pangaea list | grep account | cut -c10-51);
 echo my wallet=$wallet
 
-shardid=$(tac latest/*.log | grep -Eom1 "\"shardID\"\:[0-9]+" | awk -F: '{print $2}');
+shardid=$(tac latest/*.log | grep -Eom1 "\"shardID\"\:[0-9]+" | cut -c11-11);
 echo my shard=$shardid
 
 while true
