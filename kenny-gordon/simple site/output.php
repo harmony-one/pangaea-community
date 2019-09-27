31b9bedaa9dfd5b3d7bc46616c268a36af01ce06
kenny-gordon <44794386+kenny-gordon@users.noreply.github.com>
2019-09-27 10:50:03 +1000

    Set node path and removed un-used IP filters.

--- kenny-gordon/simple site/output.php
+++ kenny-gordon/simple site/output.php
@@ -16,32 +16,35 @@ $udate = date("Y-m-d H:i:s");
 /** Set Scope of data to fetch. */
 $shard_output = shell_exec('ls -d ' . $node_path . '/harmony_db_* | tail -1 | sed "s|' . $node_path . '/||g" | cut -c12- ');
 $block_output = shell_exec('tac ' . $node_path . '/latest/zero*.log | grep -oam 1 -E "\"(blockNumber|myBlock)\":[0-9\"]*" | grep -oam 1 -E "[0-9]+" ');
-$debug_output = shell_exec('tac ' . $node_path . '/latest/*.log  | grep -m 25 "debug" | sed "s@\b49\.176\.161\.107\b@xxx.xxx.xxx.xxx@g"  ');
-$error_output = shell_exec('tac ' . $node_path . '/latest/zero*.log  | grep -m 50 "error" | sed "s@\b49\.176\.161\.107\b@xxx.xxx.xxx.xxx@g"  ');
-$bingo_output = shell_exec('tac ' . $node_path . '/latest/zero*.log  | grep -a "BINGO" | sed "s@\b49\.176\.161\.107\b@xxx.xxx.xxx.xxx@g"  ');
+$debug_output = shell_exec('tac ' . $node_path . '/latest/*.log  | grep -m 25 "debug" ');
+$error_output = shell_exec('tac ' . $node_path . '/latest/zero*.log  | grep -m 50 "error" ');
+$bingo_output = shell_exec('tac ' . $node_path . '/latest/zero*.log  | grep -a "BINGO" ');
 $tx_output = shell_exec('tac ' . $node_path . '/transactions.log | grep -a "Transaction Id" ');
 
 
 ?>
 
 <div class="col-lg-12 mt-2">
+  
   <div class="row">
-    <div class="col-md">
-      <h1 class="text-left"><span class="badge badge-dark">Shard: <?= $shard_output ?></span></h1>
+
+    <div class="col-md mr-auto">
+      <h1 class="text-left"><span class="badge badge-dark text-wrap">Shard: <?= $shard_output ?></span></h1>
     </div>
-    <div class="col-md">
-      <h1 class="text-center"><span class="badge badge-dark">Page Refreshed: <?= $udate ?></span></h1>
+    <div class="col-md-12 mx-auto">
+      <h1 class="text-center"><span class="badge badge-dark text-wrap">Page Refreshed: <?= $udate ?></span></h1>
     </div>
-    <div class="col-md">
-      <h1 class="text-right"><span class="badge badge-dark">Block: <?= $block_output ?></span></h1>
+    <div class="col-md ml-auto">
+      <h1 class="text-right"><span class="badge badge-dark text-wrap">Block: <?= $block_output ?></span></h1>
     </div>
+
   </div>
 
   <div class="row">
-    <div class="col">
+    <div class="col-md">
 
       <!-- Card Content -->
-      <div class="card my-2 ">
+      <div class="card mt-2 ">
         <div class="card-header text-white bg-success">
           <div class="card-title my-auto">
             <div class="row">
@@ -75,7 +78,7 @@ $tx_output = shell_exec('tac ' . $node_path . '/transactions.log | grep -a "Tran
       <!-- ./Card Content -->
 
       <!-- Card Content -->
-      <div class="card my-2 bg-info">
+      <div class="card mt-2 bg-info">
         <div class="card-header text-white">
           <div class="card-title my-auto">
             <div class="row">
@@ -109,10 +112,10 @@ $tx_output = shell_exec('tac ' . $node_path . '/transactions.log | grep -a "Tran
       <!-- ./Card Content -->
 
     </div>
-    <div class="col">
+    <div class="col-md">
 
       <!-- Card Content -->
-      <div class="card my-2 bg-warning">
+      <div class="card mt-2 bg-warning">
         <div class="card-header text-white">
           <div class="card-title my-auto">
             <div class="row">
@@ -146,7 +149,7 @@ $tx_output = shell_exec('tac ' . $node_path . '/transactions.log | grep -a "Tran
       <!-- ./Card Content -->
 
       <!-- Card Content -->
-      <div class="card my-2 bg-danger">
+      <div class="card mt-2 bg-danger">
         <div class="card-header text-white">
           <div class="card-title my-auto">
             <div class="row">
