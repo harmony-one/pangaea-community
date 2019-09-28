1d80a508f7920ea0e67629091b585d834e2ca5b3
Mike Hibbert <hibbert.michael@gmail.com>
2019-09-28 09:25:17 +0100

    Updated setup.sh for correct paths to files

--- MikeHibbert/pangaea_version_monitor/setup.sh
+++ MikeHibbert/pangaea_version_monitor/setup.sh
@@ -10,16 +10,16 @@ do_yum_install() {
 
 do_apt_install
 
-sudo pip install -r `pwd`/pangaea_version_monitor/conf/requirements.txt
+sudo pip install -r `pwd`/pangaea-community/MikeHibbert/pangaea_version_monitor/conf/requirements.txt
 
 echo "Installing configuration files ..."
 mkdir `pwd`/logs
 
-rm `pwd`/pangaea_version_monitor/local_settings.py
-echo "from settings import *">> `pwd`/pangaea_version_monitor/local_settings.py
-echo "BASE_DIR = '`pwd`'" >> `pwd`/pangaea_version_monitor/local_settings.py
-sudo sed 's?BASE_PATH?'`pwd`'?' "`pwd`/pangaea_version_monitor/conf/pangaea_version_monitor.conf" > /etc/supervisor/conf.d/pangaea_version_monitor.conf
-sudo sed 's?BASE_PATH?'`pwd`'?' "`pwd`/pangaea_version_monitor/conf/pangaea_node.conf" > /etc/supervisor/conf.d/pangaea_node.conf
+rm `pwd`/pangaea-community/MikeHibbert/pangaea_version_monitor/local_settings.py
+echo "from settings import *">> `pwd`/pangaea-community/MikeHibbert/pangaea_version_monitor/local_settings.py
+echo "BASE_DIR = '`pwd`'" >> `pwd`/pangaea-community/MikeHibbert/pangaea_version_monitor/local_settings.py
+sudo sed 's?BASE_PATH?'`pwd`'?' "`pwd`/pangaea-community/MikeHibbert/pangaea_version_monitor/conf/pangaea_version_monitor.conf" > /etc/supervisor/conf.d/pangaea_version_monitor.conf
+sudo sed 's?BASE_PATH?'`pwd`'?' "`pwd`/pangaea-community/MikeHibbert/pangaea_version_monitor/conf/pangaea_node.conf" > /etc/supervisor/conf.d/pangaea_node.conf
 
 sudo supervisorctl reload
 
